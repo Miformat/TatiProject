322e2525690b9db7c718d359cf5050d35941ef6d
Miformat <m.humeau@J001-0136-12.etudiant.rubika-edu.com.local>
2016-03-06 20:24:34 +0100

    init

---
+++ Dismember Me/Assets/Script/ActionScript.cs
@@ -0,0 +1,262 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionScript : MonoBehaviour {
+
+	/*TODO*/
+	/*head velocity/gravity*/
+	/*jump*/
+	/*kick*/
+
+	public enum part
+	{
+		HEAD,
+		TORSO,
+		LEGS
+	};
+
+	ActionScript head;
+	ActionScript torso;
+	ActionScript legs;
+
+	public float distance;
+	[Header("General")]
+	public part ID;
+	public bool isActive = false;
+	[Header("Torso")]
+	public bool isSticky = false;
+	//public bool locked = false;
+
+	Vector3 speedVector;
+	Vector3 jumpVector;
+
+	float speed = 5;
+	float jump = 100;
+	float minimalDist = 2;
+
+	public bool isJumping = false;
+	public bool isGrounded = false;
+	Vector3 startPos;
+	Vector3 endPos;
+	float jumpDist = 10;
+	float direction = 1;
+	float hopeHeight = 100;
+	float timerThrow = 0;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		Settings (ID);
+		Coloring ();
+		speedVector = new Vector3 (speed, 0, 0);
+		jumpVector = new Vector3 (0, jump, 0);
+		if (ID == part.LEGS){isActive = true;}
+		ActionScript[] tabPart = GameObject.FindObjectsOfType<ActionScript>();
+		foreach(ActionScript elem in tabPart)
+		{
+			if (elem.ID == part.HEAD){head = elem;}
+			else if (elem.ID == part.TORSO){torso = elem;}
+			else if (elem.ID == part.LEGS){legs = elem;}
+		}
+	}
+
+	void Coloring()
+	{
+		switch (ID) 
+		{
+			case part.HEAD:
+				this.gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+				break;
+			case part.LEGS:
+				this.gameObject.GetComponent<Renderer>().material.color = Color.magenta;
+				break;
+			case part.TORSO:
+				this.gameObject.GetComponent<Renderer>().material.color = Color.green;
+				break;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (isJumping)
+		{
+			Jumping();
+		}
+		if (isActive)
+		{
+			if (ID == part.HEAD){this.GetComponent<Rigidbody>().velocity = Vector3.zero;}
+			if (Input.GetKeyDown(KeyCode.Alpha1)){this.Activate(1);}
+			if (Input.GetKeyDown(KeyCode.Alpha2)){this.Activate(2);}
+			if (Input.GetKeyDown(KeyCode.Alpha3)){this.Activate(3);}
+			if (Input.GetButtonDown("Fire1")) 
+			{
+				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+				RaycastHit hit;
+				if (Physics.Raycast(ray, out hit))
+				{
+					float headDist = Vector3.Distance(this.gameObject.transform.position, head.transform.position);
+					float torsoDist = Vector3.Distance(this.gameObject.transform.position, torso.transform.position);
+					if (ID == part.LEGS)
+					{
+						if (headDist < minimalDist)
+						{
+							Debug.Log("Kick head by legs at : ");
+							Vector3 hitpoint = hit.point;
+							if (hitpoint.y <= 0) {hitpoint.y = 0.5f;}
+							hitpoint.z = 0;
+							head.setJump(0,0,5,0,hitpoint);
+						}
+						if (torsoDist < minimalDist)
+						{
+							Debug.Log("Kick torso by legs at : ");
+							Vector3 hitpoint = hit.point;
+							if (hitpoint.y < 0) {hitpoint.y = 0.5f;}
+							hitpoint.z = 0;
+							torso.setJump(0,0,5,0,hitpoint);
+						}
+						//Debug.Log(Input.mousePosition);
+					}
+					if (ID == part.TORSO && headDist < minimalDist)
+					{
+						Debug.Log("Throw head by torso at : ");
+						Vector3 hitpoint = hit.point;
+						if (hitpoint.y <= 0) {hitpoint.y = 0.5f;}
+						hitpoint.z = 0;
+						head.setJump (0, 0, 5, 0, hitpoint);
+					}
+				}
+			}
+			//if (Input.GetKeyDown(KeyCode.A)){Previous();}
+			//if (Input.GetKeyDown(KeyCode.E)){Next();}
+			
+			if (Input.GetKeyDown(KeyCode.Z) && ID == part.LEGS && isGrounded)
+			{
+				setJump(jumpDist, 0, 10, 0, Vector3.zero);
+			}
+			if (Input.GetKey(KeyCode.Q) && !isJumping)
+			{
+				//if (locked && direction == -1) {} 
+				//else{
+					this.transform.position -= speedVector * Time.deltaTime;
+				//}
+				direction = -1;
+			}
+			if (Input.GetKeyDown(KeyCode.S) && ID == part.TORSO){isSticky = !isSticky;}
+			if (Input.GetKey(KeyCode.D) && !isJumping)
+			{
+				//if (locked && direction == 1) {} 
+				//else {
+					this.transform.position += speedVector * Time.deltaTime;
+				//}
+				direction = 1;
+			}
+		}
+	}
+
+	void setJump(float xOff, float yOff, float HH, float TT, Vector3 EP)
+	{
+		startPos = this.gameObject.transform.position;
+		if (EP == Vector3.zero){endPos = startPos;}
+		else{endPos = EP;}
+		endPos.x += xOff * direction;
+		endPos.y -= yOff;
+		hopeHeight = HH;
+		timerThrow = TT;
+		isGrounded = false;
+		isJumping = true;
+	}
+
+	void Jumping()
+	{
+		distance = Vector3.Distance(this.gameObject.transform.position, endPos);
+		if (distance > 0.5f)
+		{
+			float height = Mathf.Sin(Mathf.PI * timerThrow) * hopeHeight;
+			this.gameObject.transform.position = Vector3.Lerp(startPos, endPos, timerThrow) + Vector3.up * height; 
+			timerThrow += Time.deltaTime /(distance/10f);
+		}
+		/*else if(!isGrounded)
+		{
+			setJump(0.25f,0.5f,0,timerThrow, Vector3.zero);
+		}*/
+		else
+		{
+			this.gameObject.GetComponent<Rigidbody> ().velocity = Vector3.zero;
+			isJumping = false;
+		}
+	}
+
+	void OnCollisionEnter(Collision collision) 
+	{
+		if (collision.gameObject.tag == "Ground")
+		{
+			isGrounded = true;
+			this.gameObject.GetComponent<Rigidbody> ().velocity = Vector3.zero;
+		}
+		isJumping = false;
+	}
+
+	void Activate(int partID)
+	{
+		if (partID == 1 && ID != part.HEAD)
+		{
+			head.isActive = true;
+			isActive = false;
+		}
+		else if (partID == 2 && ID != part.TORSO)
+		{
+			torso.isActive = true;
+			isActive = false;
+		}
+		else if (partID == 3 && ID != part.LEGS)
+		{
+			legs.isActive = true;
+			isActive = false;
+		}
+	}
+	
+	void Settings(part parID)
+	{
+		if (parID == part.HEAD)
+		{
+			speed *= 2;
+		}
+		else if (parID == part.TORSO)
+		{
+			speed /= 2;
+		}
+		else if (parID == part.LEGS)
+		{
+			speed *= 1;
+		}
+	}
+
+	/*void Next()
+	{
+		if (ID == part.HEAD)
+		{
+			torso.isActive = true;
+			this.isActive = false;
+		}
+		else if (ID == part.TORSO)
+		{
+			legs.isActive = true;
+			this.isActive = false;
+		}
+		else if (ID == part.LEGS)
+		{
+			head.isActive = true;
+			this.isActive = false;
+		}
+	}*/
+
+	/*void Previous()
+	{
+		Debug.Log (ID);
+		if (ID == part.HEAD){legs.wait = true;legs.waitTime = waitmax;Debug.Log("legs previous");}
+		else if (ID == part.TORSO){head.wait = true;head.waitTime = waitmax;Debug.Log("head previous");}
+		else if (ID == part.LEGS){torso.wait = true;torso.waitTime = waitmax;Debug.Log("torso previous");}
+		isActive = false;
+	}*/
+}
---
+++ Dismember Me/Assets/Script/MechanicScript.cs
@@ -0,0 +1,209 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MechanicScript : MonoBehaviour {
+
+	/*TODO*/
+	/*platform child movment*/
+	/*turning platform*/
+	/*end platform*/
+
+	public enum MechaType
+	{
+		TRIGGER,
+		DOOR,
+		HPLAT,
+		VPLAT,
+		TPLAT,
+		END
+	};
+
+	[Header("General")]
+	public MechaType type;
+	public int ID;
+	public int nbAffected;
+	public bool isActive;
+	[Header("Platform")]
+	public float range;
+	public float speed;
+
+	MechanicScript[] affectedMecha;
+	Vector3 startPos;
+	GameObject actor = null;
+
+	// Use this for initialization
+	void Start () 
+	{
+		startPos = this.gameObject.transform.position;
+		Coloring ();
+		if (nbAffected != 0)
+		{
+			affectedMecha = new MechanicScript[nbAffected];
+			MechanicScript[] tempMecha = GameObject.FindObjectsOfType<MechanicScript> ();
+			int index = 0;
+			foreach(MechanicScript current in tempMecha)
+			{
+				if (current.ID == ID && current != this.gameObject.GetComponent<MechanicScript>())
+				{
+					affectedMecha[index] = current;
+					index++;
+				}
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Vector3.Distance(this.gameObject.transform.position,startPos) > range){speed = -speed;}
+		if (isActive)
+		{
+			if(type == MechaType.HPLAT){this.gameObject.transform.position += new Vector3(speed,0,0) * Time.deltaTime;}
+			if(type == MechaType.VPLAT){this.gameObject.transform.position += new Vector3(0,speed,0) * Time.deltaTime;}
+			if(type == MechaType.DOOR)
+			{
+				this.gameObject.GetComponent<BoxCollider>().enabled = false;
+				this.gameObject.GetComponent<MeshRenderer>().enabled = false;
+			}
+		}
+		else
+		{
+			if(type == MechaType.DOOR)
+			{
+				this.gameObject.GetComponent<BoxCollider>().enabled = true;
+				this.gameObject.GetComponent<MeshRenderer>().enabled = true;
+			}
+		}
+	}
+
+	void Coloring()
+	{
+		switch(ID)
+		{
+		case 0:
+			this.gameObject.GetComponent<Renderer>().material.color = Color.red;
+			break;
+		case 1:
+			this.gameObject.GetComponent<Renderer>().material.color = Color.blue;
+			break;
+		case 2:
+			this.gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+			break;
+		case 3:
+			this.gameObject.GetComponent<Renderer>().material.color = Color.green;
+			break;
+		case 4:
+			this.gameObject.GetComponent<Renderer>().material.color = Color.cyan;
+			break;
+		case 5:
+			this.gameObject.GetComponent<Renderer>().material.color = Color.magenta;
+			break;
+
+		}
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (type == MechaType.TRIGGER && other.GetComponent<ActionScript> () && actor == null)
+		{
+			actor = other.gameObject;
+			foreach(MechanicScript current in affectedMecha)
+			{
+				current.isActive = true;
+			}
+		}
+	}
+
+	void OnTriggerStay(Collider other) 
+	{
+		if (type == MechaType.TRIGGER && other.GetComponent<ActionScript> () && actor == null)
+		{
+			actor = other.gameObject;
+			foreach(MechanicScript current in affectedMecha)
+			{
+				current.isActive = true;
+			}
+		}		
+	}
+
+	void OnTriggerExit(Collider other) 
+	{
+		if (type == MechaType.TRIGGER && other.GetComponent<ActionScript> () && other.gameObject == actor)
+		{
+			actor = null;
+			foreach(MechanicScript current in affectedMecha)
+			{
+				current.isActive = false;
+			}
+		}
+	}
+
+	void Dading(GameObject dad, GameObject child, bool enter)
+	{
+		Quaternion rot = Quaternion.identity;//child.transform.rotation;
+		Vector3 scale = Vector3.zero;
+		if (enter) 
+		{
+			scale.x = child.transform.localScale.x / dad.transform.localScale.x;
+			scale.y = child.transform.localScale.y / dad.transform.localScale.y;
+			scale.z = child.transform.localScale.z / dad.transform.localScale.z;
+			child.transform.parent = dad.transform;
+		}
+		else 
+		{
+			scale.x = child.transform.localScale.x * dad.transform.localScale.x;
+			scale.y = child.transform.localScale.y * dad.transform.localScale.y;
+			scale.z = child.transform.localScale.z * dad.transform.localScale.z;
+			child.transform.parent = null;
+		}
+		child.transform.rotation = rot;
+		child.transform.localScale = scale;
+	}
+
+	void OnCollisionEnter(Collision other) 
+	{
+		if (type == MechaType.HPLAT && other.gameObject.GetComponent<ActionScript> ().ID != ActionScript.part.TORSO) 
+		{
+			Dading (this.gameObject, other.gameObject, true);
+		}
+		else if (other.gameObject.GetComponent<ActionScript> ().isSticky && other.gameObject.GetComponent<ActionScript> ().ID == ActionScript.part.TORSO) 
+		{
+			Debug.Log ("1");
+			Dading (this.gameObject, other.gameObject, true);
+		}
+	}
+
+	void OnCollisionStay(Collision other) 
+	{
+		if (other.gameObject.GetComponent<ActionScript> ().isSticky && other.gameObject.transform.parent != this.gameObject.transform && other.gameObject.GetComponent<ActionScript> ().ID == ActionScript.part.TORSO) 
+		{
+			Debug.Log ("2");
+			Dading (this.gameObject, other.gameObject, true);
+			//other.gameObject.GetComponent<ActionScript> ().locked = false;
+		}
+		else if (other.gameObject.GetComponent<ActionScript> ().isSticky == false && other.gameObject.transform.parent == this.gameObject.transform && other.gameObject.GetComponent<ActionScript> ().ID == ActionScript.part.TORSO) 
+		{
+			Debug.Log ("3");
+			Dading (this.gameObject, other.gameObject, false);
+			//other.gameObject.GetComponent<ActionScript> ().locked = false;
+		}
+	}
+
+	void OnCollisionExit(Collision other) 
+	{
+		if (type == MechaType.HPLAT && other.gameObject.GetComponent<ActionScript> ().ID != ActionScript.part.TORSO) 
+		{
+			Dading (this.gameObject, other.gameObject, false);
+		}
+		else if (other.gameObject.GetComponent<ActionScript> ().isSticky == false && other.gameObject.transform.parent == this.gameObject.transform && other.gameObject.GetComponent<ActionScript> ().ID == ActionScript.part.TORSO) 
+		{
+			Debug.Log ("4");
+			Dading (this.gameObject, other.gameObject, false);
+		}
+		else if (other.gameObject.GetComponent<ActionScript> ().isSticky && other.gameObject.transform.parent == this.gameObject.transform && other.gameObject.GetComponent<ActionScript> ().ID == ActionScript.part.TORSO) 
+		{
+			Debug.Log ("5");
+			//other.gameObject.GetComponent<ActionScript> ().locked = true;
+		}
+	}
+}
